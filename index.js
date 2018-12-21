118f5685d2210516415dbf7d9087dcf2a61e8f92
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-22 00:32:56 +0300

    min: заглушка на POST запрос

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/send', (req, res) => {
 	let message = req.query.template;
 });
+app.post('/send', (req, res) => {
+	let message = req.query.template;
+});
 
 app.listen(3000, () => {
   console.log('Vk notifications service listening on port 3000!');
