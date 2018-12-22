743fe40a61f7dd7d239cddc94f654acc402f75eb
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-22 18:39:25 +0300

    feat: прототип отправки уведомлений

--- index.js
+++ index.js
@@ -5,6 +5,80 @@ let mongo = require('./modules/mongo')
 let VK = require('./mock/vk-api');
 let app = express();
 
+
+let page = 0;
+let nPerPage = 50;
+let nPerPageDelta = 0;
+function updatePage(delta){
+	if(nPerPageDelta + delta > nPerPage){
+		nPerPageDelta = nPerPage - nPerPageDelta
+	}
+}
+
+(async()=>{
+	// получение данных из бд
+	// mongo.clear('players');
+	// mongo.clear('received');
+	// let count = await mongo.getCountFrom('received');
+	let playersIds = await mongo.getIdsFrom(page, nPerPage, 'players');
+	// let receivedIds = await mongo.getIdsFrom(0, 10, 'received');
+	// let insertedPlayers = await mongo.insertMockTo(1000000, 'players');
+	// let insertedReceived = await mongo.insertMockTo(10, 'received');
+	let findedPlayersInReceived = await mongo.find(playersIds, 'received');
+	// console.log(count);
+	console.log(playersIds);
+	// console.log(insertedPlayers);
+	// console.log(receivedIds);
+	// console.log(inserted);
+	console.log(findedPlayersInReceived);
+
+
+	// исключение из списка рассылки тех, тко присутствует в бд получивших
+	let idsNotification = [];
+	let deletedCount = 0;
+	for(let i = 0; i < findedPlayersInReceived.length; i++){
+		for(let j = 0; j < playersIds.length; j++){
+			if(findedPlayersInReceived[i].id == playersIds[j].id){
+				playersIds.splice(j, 1);
+				deletedCount++;
+			}
+		}
+	}
+	console.log(playersIds);
+	console.log(deletedCount);
+
+
+
+	// отправка сообщений
+	VK.sendNotification(playersIds, 'message')
+		.then(response => {
+			(async()=>{
+				console.log('Send successful');
+				console.log(response);
+				// Сохранение ids которым удалось отправить оповещение
+				let insertedReceived = await mongo.insert(response, 'received');
+				console.log('Save received');
+				console.log(insertedReceived);
+				logger.info(`Successful notification for: ${JSON.stringify(response)}`);
+			})()
+		}).catch(err=>{
+			console.log(err);
+			logger.error(err);
+		})
+})()
+
+// let page = 0;
+// let nPerPage = 5;
+// let interval = 3000;
+// function getIds(){
+// 	mongo.getIds(page, nPerPage).then(res=>{
+// 		console.log(res);
+// 		page++;
+// 		setTimeout(getIds, interval);
+// 	}).catch(err=>{console.log(err)});
+// }
+// getIds();
+
 app.get('/send', (req, res) => {
 	let message = req.query.template;
 
@@ -12,6 +86,7 @@ app.get('/send', (req, res) => {
 		VK.sendNotification(ids, message)
 			.then(response=>{
 				logger.info(`successful notification for: ${JSON.stringify(response)}`);
+				mongo.insertReceived(ids);
 			})
 			.catch(error=>{
 				logger.error(error);
--- modules/mongo.js
+++ modules/mongo.js
@@ -5,21 +5,85 @@ const mongoClient = new MongoClient(config.host, { useNewUrlParser: true });
 const {url, dbname} = config;
 
 const dbcollection = 'players';
+const dbcollection2 = 'received';
+
 const data = {id: 1, first_name: 'Ivan'};
+const datas = [];
 const query = {name: 'Ivan'};
 
 const mongo = {
-	async getIds(collection){
-		mongoClient.connect(function(err, client){
-		  if(err) return console.log(err);
-		   client.db(dbname).collection(dbcollection).updateMany(query, {$set: data}, {upsert: true}, (res) => {
-		   	console.log(res);
-				client.close();
-			});
-		});
+	clear(_collection){
+		return new Promise((res, rej)=>{
+			mongoClient.connect((err, client)=>{
+				let db = client.db(dbname);
+				let collection = db.collection(_collection);
+				collection.drop().then(result=>{
+					res(result);
+				}); 
+			})
+		})
+	},
+	async getCountFrom(_collection) {
+		let client = await mongoClient.connect();
+		let db = client.db(dbname);
+		let collection = db.collection(_collection);
+		try {
+			return await collection.countDocuments()
+		} finally {
+			// client.close();
+		}
+	},
+	async getIdsFrom(page, nPerPage, _collection){
+		let client = await mongoClient.connect();
+		let db = client.db(dbname);
+		let collection = db.collection(_collection);
+		try {
+			// TODO брать только id
+			return await collection.find().skip(page * nPerPage).limit(nPerPage).toArray(); 
+		} finally {
+			// client.close();
+		}
+	},
+	async find(query, _collection){
+		let _qr = query.map(element => {return element.id});
 
-		let ids = [{id:'1'}, {id:'2'}, {id:'3'}];
-		return ids;
+		let client = await mongoClient.connect();
+		let db = client.db(dbname);
+		let collection = db.collection(_collection);
+		try {
+			let query = { id: { $in: _qr } };
+			let projection = { id: '' };
+			return await collection.find(query, projection).toArray();
+		} finally {
+			client.close();
+		}
+	},
+	async insert (ids, _collection) {
+		let client = await mongoClient.connect();
+		let db = client.db(dbname);
+		let collection = db.collection(_collection);
+		try {
+			// TODO сохранять только id
+			return await collection.insertMany(ids);
+		} finally {
+			client.close();
+		}
+	},
+
+	async insertMockTo(count, _collection){
+		for(let i = 0; i < count; i++){
+			let _data = {id: i, first_name: 'Ivan'};
+			datas.push(_data);
+		}
+
+		let client = await mongoClient.connect();
+		let db = client.db(dbname);
+		let collection = db.collection(_collection);
+		try {
+			return await collection.insertMany(datas);
+		} finally {
+			client.close();
+		}
 	}
 }
 
