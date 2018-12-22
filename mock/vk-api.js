883d31f59b5eab03fc090df685a4bd94686dcff0
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-22 05:25:27 +0300

    feat: сброс счетчика запросов к vk-api

--- mock/vk-api.js
+++ mock/vk-api.js
@@ -1,5 +1,38 @@
+const config = require('./../config').vk;
+
+const { maxUsersIdsCount, maxMessageLength, maxRequestsRate } = config.notice;
+
+let requestNumber = 0;
+let requestNumberId = '';
+
+function resetRequestNumber(){
+	if(requestNumberId == ''){
+		requestNumberId = setTimeout(()=>{
+			requestNumber = 0;
+			requestNumberId = '';
+			console.log('reset');
+		}, 1000)
+	}
+};
+
 const VK = {
 	async sendNotification(ids = [], text = ''){
+		if(requestNumber >= maxRequestsRate) {
+			// console.error('Too frequently');
+			return;
+		}
+		if(ids.length > maxUsersIdsCount || text > maxMessageLength) {
+			// console.error('Invalid data');
+			return;
+		}
+		if(false) {
+			// console.error('Server fatal error');
+			return;
+		}
+
+		requestNumber++;
+		resetRequestNumber();
+
 		let response = [{id:''}, {id:''}, {id:''}];
 		return response;
 	}
