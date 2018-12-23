b01b3eef257f6f5cc3dfe5162be6ae1f47811e38
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-23 12:54:41 +0300

    feat: состояние хранится в модуле состояния

--- modules/state.js
+++ modules/state.js
@@ -5,19 +5,26 @@ const mongoClient = new MongoClient(config.host, { useNewUrlParser: true });
 const {url, dbname} = config;
 
 const dbcoll = 'state';
-let db = {};
+let db;
 
 const state = {
+	status: '',
+	msg: '',
 	async connect(){
 		client = await mongoClient.connect();
 		db = client.db(dbname);
 		return db;
 	},
 	async save(_state){
+		this.status = _state.status;
+		this.msg = _state.msg;
 		return await db.collection(dbcoll).updateOne({}, {$set: _state}, {upsert: true});
 	},
 	async load(){
-		return await db.collection(dbcoll).findOne()
+		let _state = await db.collection(dbcoll).findOne();
+		this.status = _state.status;
+		this.msg = _state.msg;
+		return _state;
 	},
 }
 
