ac6c1741e537433d0e4506f4f8ba680120ad3c9c
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-26 03:16:34 +0300

    refactor: sender переименован в context

--- modules/notice-sender/states.js
+++ modules/notice-sender/states.js
@@ -13,15 +13,15 @@ let playersIds = [];
 let sendingInterval = new TimeInterval(delayBetweenRequests, delayBetweenErrors);
 
 const idleState = {
-	async action (sender) {}
+	async action (context) {}
 }
 
 const initializeState = {
-	async action (sender) /*sender -> context*/ {
+	async action (context) /*sender -> context*/ {
 		state.connect().then(() => {
 			state.load().then(res => {
-				sender.setState(connectionState);
-				sender.action();
+				context.setState(connectionState);
+				context.action();
 			}).catch(err => {
 				state.save({ status: states.IDLE, msg: '', offset: 0 });
 			})
@@ -31,39 +31,39 @@ const initializeState = {
 
 // Состяние подключения
 const connectionState = {
-	async action (sender) {
+	async action (context) {
 		await repository.connect();
 		await repository.skipPlayersIdsQuantity(state.offset);
 
 		if(state.status == states.SENDING){
-			sender.setState(processingState);
+			context.setState(processingState);
 		}else if(state.status == states.ERROR){
-			sender.setState(processingState);
+			context.setState(processingState);
 		}else if(state.status == states.IDLE){
-			sender.setState(idleState);
+			context.setState(idleState);
 		}
 
-		sender.action();
+		context.action();
 	}
 }
 
 const processRequestState = {
-	async action (sender) {
+	async action (context) {
 		clearTimeout(sendingState.timeoutID);
 		clearImmediate(sendingState.immediateID);
-		state.save({ status: state.status, msg: sender.message, offset: state.offset});
+		state.save({ status: state.status, msg: context.message, offset: state.offset});
 
 		if(state.status == states.SENDING){
-			await state.save({ status: states.SENDING, msg: sender.message, offset: 0});
-			await sender.setState(cleaningState);
+			await state.save({ status: states.SENDING, msg: context.message, offset: 0});
+			await context.setState(cleaningState);
 		}else if(state.status == states.ERROR){
-			await sender.setState(connectionState);
+			await context.setState(connectionState);
 		}else if(state.status == states.IDLE){
-			await state.save({ status: states.SENDING, msg: sender.message, offset: 0});
-			await sender.setState(connectionState);
+			await state.save({ status: states.SENDING, msg: context.message, offset: 0});
+			await context.setState(connectionState);
 		}
 
-		sender.action();
+		context.action();
 	}
 }
 
@@ -71,11 +71,11 @@ const processRequestState = {
 // Состояние очистки списка получивших уведомление, переход в это 
 // состояние при запросе на новую рассылку
 const cleaningState = {
-	async action (sender) {
+	async action (context) {
 		await repository.resetPlayersIdsCursor();
 		await repository.clearReceivedIds();
-		sender.setState(processingState);
-		sender.action();
+		context.setState(processingState);
+		context.action();
 	}
 }
 
@@ -83,14 +83,14 @@ const cleaningState = {
 // из players ids, проверка на конец коллекции, сравнение полученных id 
 // с теми, что в коллекции получивших.
 const processingState = {
-	async action (sender) {
+	async action (context) {
 		// TODO вынести наверх
 		let playersCount = await repository.getPlayersIdsCount();
 		let delta = playersCount - state.offset;
 
 		if(delta <= 0){
-			sender.setState(endState);
-			sender.action();
+			context.setState(endState);
+			context.action();
 			return;
 		}
 
@@ -105,8 +105,8 @@ const processingState = {
 		// Отказ от поиска plyaers ids в received с целью оптимизации
 		// await repository.subtractReceivedFromPlayers(playersIds);
 
-		sender.setState(sendingState);
-		sender.action();
+		context.setState(sendingState);
+		context.action();
 	}
 }
 
@@ -116,53 +116,51 @@ const sendingState = {
 	immediateID: '',
 	timeoutID: '',
 
-	async action (sender) {
+	async action (context) {
 		VK.sendNotification(playersIds, state.msg)
 			.then(response => {
-				(async () => {
-					sender.setState(processingState);
-					logger.info(`Sending successful ${ state.msg } to ${ JSON.stringify(response) }`);
+				context.setState(processingState);
+				logger.info(`Sending successful ${ state.msg } to ${ JSON.stringify(response) }`);
 
-					// Отказ от сохранения plyaers ids в received с целью оптимизации
-					// await repository.saveReceivedIds(response);
-					await state.save({ status: states.SENDING, msg: state.msg, offset: state.offset += idsToTake } );
-					sendingInterval.fast();
-				})()
+				// Отказ от сохранения plyaers ids в received с целью оптимизации
+				// await repository.saveReceivedIds(response);
+				state.save({ status: states.SENDING, msg: state.msg, offset: state.offset += idsToTake } );
+				sendingInterval.fast();
 			}).catch( err => {
 				if(err.message == 'Invalid data'){
-					sender.setState(processingState);
+					context.setState(processingState);
 					logger.error(`Invalid data, failed send ${ state.msg } to : ${ JSON.stringify(playersIds) }`);
 					state.save({ status: state.status, msg: state.msg, offset: state.offset });
 					sendingInterval.slow();
 				}else if(err.message == 'Too frequently'){
-					sender.setState(processingState);
+					context.setState(processingState);
 					logger.error(`Too frequently, failed send ${ state.msg } to : ${ JSON.stringify(playersIds) }`);
 					sendingInterval.slow();
 				}else if(err.message == 'Server fatal error'){
-					sender.setState(disconnectState);
+					context.setState(disconnectState);
 					state.save({ status: states.ERROR, msg: state.msg, offset: state.offset });
 					logger.error(`Server fatal error, failed send ${ state.msg } to : ${ JSON.stringify(playersIds) }`);
 				}
 			});
 
 		this.immediateID = setImmediate(()=>{
-			this.timeoutID = setTimeout(()=>{ clearTimeout(this.timeoutID); sender.action(); }, sendingInterval.time);
+			this.timeoutID = setTimeout(()=>{ clearTimeout(this.timeoutID); context.action(); }, sendingInterval.time);
 		})
 	}
 }
 
 // Состояние завершения рассылки
 const endState = {
-	async action (sender) {
+	async action (context) {
 		logger.info(`Notification sending complete`);
 		await state.save({ status: states.IDLE, msg: '', offset: 0 });
-		sender.setState(disconnectState);
-		sender.action();
+		context.setState(disconnectState);
+		context.action();
 	}
 }
 
 const disconnectState = {
-	async action (sender) {
+	async action (context) {
 		await repository.disconnect();
 	}
 }
